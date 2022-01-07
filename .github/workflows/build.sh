046581acf0b3a62690bda4b90bc9e9381ea42586
metworkbot <metworkbot@metwork-framework.org>
2022-01-07 09:20:02 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -15,9 +15,7 @@ export DRONE=true
 rm -rf html_doc rpms .build_hash
 
 
-
-    if test "${OS_VERSION}" = "centos6"; then export METWORK_BUILD_OS=generic; else export METWORK_BUILD_OS=${OS_VERSION}; fi
-
+    if test "${OS_VERSION}" = "centos8"; then export METWORK_BUILD_OS=generic; else export METWORK_BUILD_OS=${OS_VERSION}; fi
 
 
     yum install -y metwork-mfext-layer-python3_scientific-${BRANCH##release_} metwork-mfext-layer-python2_scientific-${BRANCH##release_} metwork-mfext-layer-python2_devtools-${BRANCH##release_}
